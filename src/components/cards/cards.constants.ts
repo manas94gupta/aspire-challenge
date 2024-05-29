8dd4b10b088cf4ff5c19de61512fcbf50e9c8b4e
Manas Gupta <manas94gupta@gmail.com>
2024-05-29 16:42:52 +0530

    add card actions

---
+++ src/components/cards/cards.constants.ts
@@ -0,0 +1,27 @@
+export const CARD_ACTIONS = [
+  {
+    label: 'Freeze Card',
+    iconPath: '/assets/images/icons/freeze.png',
+    clickAction: 'freeze',
+  },
+  {
+    label: 'Set Limit',
+    iconPath: '/assets/images/icons/spend-limit.png',
+    clickAction: 'set-limit',
+  },
+  {
+    label: 'Add to GPay',
+    iconPath: '/assets/images/icons/gpay.png',
+    clickAction: 'gpay',
+  },
+  {
+    label: 'Replace Card',
+    iconPath: '/assets/images/icons/replace.png',
+    clickAction: 'replace',
+  },
+  {
+    label: 'Cancel Card',
+    iconPath: '/assets/images/icons/cancel.png',
+    clickAction: 'cancel',
+  },
+];
--- src/components/cards/tabbed-layout.tsx
+++ src/components/cards/tabbed-layout.tsx
@@ -1,6 +1,11 @@
+import Image from 'next/image';
+
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '~/components/ui/tabs';
 import { Card, CardContent } from '~/components/ui/card';
 import { PayCardsCarousel } from '~/components/cards/pay-cards-carousel';
+import { Button } from '~/components/ui/button';
+
+import { CARD_ACTIONS } from './cards.constants';
 
 export function CardsTabbedLayout() {
   return (
@@ -14,6 +19,26 @@ export function CardsTabbedLayout() {
           <CardContent className="grid grid-cols-[minmax(0,_415px)_minmax(50%,_1fr)] gap-11 px-10 py-8">
             <div className="flex flex-col gap-8">
               <PayCardsCarousel />
+
+              {/* <div className="flex justify-around rounded-2xl bg-primary/20"> */}
+              <div className="bg-primary-muted grid grid-cols-5 items-baseline justify-around rounded-2xl p-5">
+                {CARD_ACTIONS.map((action) => (
+                  <Button
+                    key={action.label}
+                    variant="ghost"
+                    className="flex h-auto flex-col gap-2 text-wrap text-xs text-card-foreground hover:bg-transparent hover:text-inherit"
+                    // onClick={() => console.log(action.clickAction)}
+                  >
+                    <Image
+                      src={action.iconPath}
+                      width={32}
+                      height={32}
+                      alt="Aspire"
+                    />
+                    <span>{action.label}</span>
+                  </Button>
+                ))}
+              </div>
             </div>
             <div>Details</div>
           </CardContent>
